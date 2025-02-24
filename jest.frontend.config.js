1dbb72b6efbc89970de888bf48806fbb05366332
Eugene <ekweirui@gmail.com>
2025-02-24 23:28:23 +0800

    Modify configuration files to run newly added testcases

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,21 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
-
+  testMatch: [
+    "<rootDir>/client/src/context/*.test.js",
+    "<rootDir>/client/src/hooks/*.test.js",
+    "<rootDir>/client/src/pages/Auth/*.test.js",
+    "<rootDir>/client/src/pages/user/*.test.js",
+    "<rootDir>/client/src/pages/*.test.js"
+  ],
+  
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: [
+    "client/src/context/**",
+    "client/src/hooks/**",
+    "client/src/pages/**",
+  ],
   coverageThreshold: {
     global: {
       lines: 100,
