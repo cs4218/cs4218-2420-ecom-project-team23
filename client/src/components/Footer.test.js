772b5a5da877c00ba4498c6bb78775082c999c9d
guowei42 <65711401+guowei42@users.noreply.github.com>
2025-03-05 13:35:56 +0800

    add Footer tests

---
+++ client/src/components/Footer.test.js
@@ -0,0 +1,39 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Footer from "./Footer";
+
+describe("Footer Component", () => {
+  const renderComponent = () => {
+    return render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    );
+  };
+
+  test("renders Footer component", () => {
+    renderComponent();
+
+    // note \u00A9 is the unicode for copyright
+    expect(
+      screen.getByText("All Rights Reserved \u00A9 TestingComp")
+    ).toBeInTheDocument();
+
+    expect(screen.getByText("About")).toBeInTheDocument();
+    expect(screen.getByText("Contact")).toBeInTheDocument();
+    expect(screen.getByText("Privacy Policy")).toBeInTheDocument();
+  });
+
+  test("renders correct links", () => {
+    renderComponent();
+
+    const aboutLink = screen.getByRole("link", { name: /about/i });
+    const contactLink = screen.getByRole("link", { name: /contact/i });
+    const policyLink = screen.getByRole("link", { name: /privacy policy/i });
+
+    expect(aboutLink).toHaveAttribute("href", "/about");
+    expect(contactLink).toHaveAttribute("href", "/contact");
+    expect(policyLink).toHaveAttribute("href", "/policy");
+  });
+});
