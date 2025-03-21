f0c7c61f7305727cc7e259b0b93c0ed158bc9fd6
guowei42 <65711401+guowei42@users.noreply.github.com>
2025-03-21 17:36:54 +0800

    add private route ui test

---
+++ tests/components/routes/Private.spec.js
@@ -0,0 +1,33 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+const loginUser = async (page) => {
+  await page.goto("http://localhost:3000");
+  await page.getByRole("link", { name: "Login" }).click();
+  await page
+    .getByRole("textbox", { name: /Enter Your Email/i })
+    .fill("cs4218@test.com");
+  await page
+    .getByRole("textbox", { name: /Enter Your Password/i })
+    .fill("cs4218@test.com");
+  await page.getByRole("button", { name: "LOGIN" }).click();
+
+  await expect(page.getByText(/login successfully/i)).toBeVisible();
+};
+
+test.describe("Private Route Component UI Tests", () => {
+  test("should have the spinner when not logged in", async ({ page }) => {
+    await page.goto("http://localhost:3000/dashboard");
+
+    await page.waitForTimeout(1000);
+
+    await expect(page.locator(".spinner-border .visually-hidden")).toHaveText("Loading...")
+  });
+
+  test("should redirect when user is logged in", async ({ page }) => {
+    await loginUser(page);
+    await page.goto("http://localhost:3000/dashboard/user"); // path goes through private then dashboard
+
+    await expect(page).toHaveURL("http://localhost:3000/dashboard/user")
+  });
+});
